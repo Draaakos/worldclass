e963e759e607c9f67d91797d3673d482f0847351
Drakos <orlando.andaur.c@gmail.com>
2022-11-13 19:40:19 -0300

    home refartor

--- front/js/modules/Home/index.jsx
+++ front/js/modules/Home/index.jsx
@@ -1,11 +1,25 @@
 import Login from './components/Login';
 
+const Banner = () => (
+  <div className="banner">
+    <div className="banner__container">
+      <div className="banner__title">Construyendo juntos</div>
+      <div className="banner__title-paragraph">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia optio aspernatur est eum adipisci nobis facere, deserunt inventore dolores neque sint mollitia cupiditate animi nam natus pariatur doloribus</div>
+    </div>
+  </div>
+);
+
 
 const Home = () => {
   return(
-    <div>
-      <Login />
-    </div>
+    <section className="page">
+      <div className="banner__wrapper">
+        <div className="banner__wrapper-shadow">
+          <Banner />
+        </div>
+      </div>
+      <div className="main-info">aca</div>
+    </section>
   )
 }
 
