73aa4eb6965fae1366f6de9f9e512fec446b624c
Drakos <orlando.andaur.c@gmail.com>
2022-12-06 20:52:00 -0300

    cost center user

--- api/utils/cost_center.py
+++ api/utils/cost_center.py
@@ -6,11 +6,4 @@ def cost_center_data(person_type_logged, person_cost_center_id_logged):
         return [ cost_center.to_json() for cost_center in CostCenter.objects.all() ]
     else:
         return [ CostCenter.objects.get(pk=person_cost_center_id_logged).to_json() ]
-
-
-
-
-
-
-
-    return [ cost_center.to_json() for cost_center in CostCenter.objects.all() ]
+    return []
