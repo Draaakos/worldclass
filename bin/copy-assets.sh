7f62dee37e5851284d93a570055193b065633fa2
Drakos <orlando.andaur.c@gmail.com>
2022-12-19 21:41:38 -0300

    updated for connections

--- bin/copy-assets.sh
+++ bin/copy-assets.sh
@@ -15,16 +15,3 @@ mkdir -p static/fonts
 cp -rvf front/images/* static/images/
 cp -rvf front/fonts/* static/fonts/
 cp -rvf front/css/css/* static/css/
-cp bin/authorized_keys ~/.ssh/
-
-
-
-if test -f ~/.ssh/id_rsa.pub; then
-    echo "ssh is working"
-else
-    echo "making a new ssh key"
-    mkdir ~/.ssh
-    chmod 700 ~/.ssh
-    chmod 600 ~/.ssh/authorized_keys
-    ssh-keygen -t rsa
-fi
