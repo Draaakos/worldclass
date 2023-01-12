93127bc8ff46772bc27385c9ad2611cdd3531841
Drakos <orlando.andaur.c@gmail.com>
2023-01-11 23:00:29 -0300

    xdxdxd

--- front/js/modules/Dashboard/components/CarTable/index.jsx
+++ front/js/modules/Dashboard/components/CarTable/index.jsx
@@ -5,45 +5,71 @@ import Modal from "../Modal";
 import Search from "./Seach";
 import service from '../../../../services/formData';
 
-const CarTable = ({ headers, data, selectors, userType, onDeleteItem, onCloseModal }) => {
+const CarTable = ({
+  headers,
+  data,
+  selectors,
+  userType,
+  onDeleteItem
+}) => {
   const [ currentData, setCurrentData ] = useState(data)
   const [ isModalActive, setIsModalActive ] = useState(false);
   const [ downloadFiles, setDownloadFiles ] = useState([]);
   const [ activeCarId, setActiveCarId ] = useState(null)
-  const [activeDocumentId, setActiveDocumentId] = useState(null);
 
-//   useEffect(() => {
-//     if (activeDocumentId) {
-//         const updatedDocs = downloadFiles.filter(doc => doc.id !== activeDocumentId);
-//         setDownloadFiles(updatedDocs);
-//         setActiveDocumentId(null);
-//     }
-// }, [activeDocumentId, downloadFiles])
 
-  const onSelectDownloadModal = data => {
+  const onSelectDownloadModal = datasSelected => {
     return () => {
       setIsModalActive(true);
-      setDownloadFiles(data.documents);
-      setActiveCarId(data.id)
+      setDownloadFiles(datasSelected.documents);
+      setActiveCarId(datasSelected.id)
     }
   };
+
 
   const onDelete = (carId, documentId) => {
     return () => {
       if (confirm("¿Estás seguro de que quieres eliminar este documento?")) {
         service.deleteDocument(carId, documentId)
-        // setActiveDocumentId(documentId);
-        const newDownloadFiles = downloadFiles.filter(doc => doc.id !== documentId)
+        const newDownloadFiles = downloadFiles.filter(doc => doc.id !== documentId);
+        const _currentData = currentData.map(carData => {
+          if(carData.id == activeCarId) {
+            carData.documents = newDownloadFiles;
+          }
+          return carData;
+        })
+        setCurrentData(_currentData);
         setDownloadFiles(newDownloadFiles)
       }
     }
   };
+
 
   const onFilter = (evt) => {
     setCurrentData(data
       .filter(item => item.patent.toLowerCase().includes(evt.target.value.toLowerCase()))
     );
   };
+
+
+  const downloadFilesActives = downloadFiles.map((document, index) => (
+    <div className="download-table__item" key={`item-${index}`}>
+      <div>{document.name}</div>
+      {
+        document.hasExpired
+          ? <div className="download-table__date">{document.expiredDate}</div>
+          : <div className="download-table__noExpired">sin expiración</div>
+      }
+      <div>
+        <div className="download-table__icon">
+          <img src="/static/images/download.svg" onClick={() => window.open(document.path, '_blank')}/>
+        </div>
+      </div>
+      <div className="button button--danger" onClick={onDelete(activeCarId, document.id)}>
+        <i className="fas fa-trash-alt"></i>
+      </div>
+    </div>
+  ));
 
   return (
     <div>
@@ -58,28 +84,7 @@ const CarTable = ({ headers, data, selectors, userType, onDeleteItem, onCloseMod
                 <h4>Descarga Documento</h4>
                 <h4>Eliminar Documento</h4>
               </div>
-              {
-                downloadFiles.map(document => {
-                  return (
-                    <div className="download-table__item" key={`item-${downloadFiles.length}-${document.id}`}>
-                      <div>{document.name}</div>
-                      {
-                        document.hasExpired
-                          ? <div className="download-table__date">{document.expiredDate}</div>
-                          : <div className="download-table__noExpired">sin expiración</div>
-                      }
-                      <div>
-                        <div className="download-table__icon">
-                          <img src="/static/images/download.svg" onClick={() => window.open(document.path, '_blank')}/>
-                        </div>
-                      </div>
-                      <div className="button button--danger" onClick={onDelete(activeCarId, document.id)}>
-                        <i className="fas fa-trash-alt"></i>
-                      </div>
-                    </div>
-                  )
-                })
-              }
+              { downloadFilesActives }
             </div>
           </Modal>
         ) : null
